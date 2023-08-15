ed68a981ca8955a598682b17dfafe96c9b18ac15
kkSayy <126177985+kkSayy@users.noreply.github.com>
2023-08-15 08:01:37 -0300

    Add files via upload

---
+++ calc_media/index2.js
@@ -0,0 +1,15 @@
+Function somar(){
+
+var media = document.getElementByld('media')
+varbr1 = document.getElementByld('barra1')
+varbr2 = document.getElementByld('barra2')
+varbr3 = document.getElementByld('barra3')
+varbr1 = Number(br1.value)
+varbr2 = Number(br2.value)
+varbr3 = Number(br3.value)
+varvalor = (br1 * 2 + br2 * 3 + br3 * 5)/10
+document.getElementById('')
+
+
+
+}
